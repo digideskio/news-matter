81cafe7e0a2c3a38d229b236e40580d70ddb7027
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-07-14 11:26:32 +0100

    Add index.js - import and expose Matter components

---
+++ matter/index.js
@@ -0,0 +1,16 @@
+// ux-news-matter/index.js
+//
+
+import contributor from './molecules/contributor/contributor';
+import metadata from './molecules/metadata/metadata';
+
+export default {
+  atoms: {},
+
+  molecules: {
+    contributor: contributor,
+    metadata: metadata,
+  },
+
+  organisms: {},
+};
